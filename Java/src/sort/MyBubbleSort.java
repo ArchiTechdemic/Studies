c1db18df3ab19a13c39cbf6e3c57b119e160e0ec
ArchiTechdemic <architect@techdemic.com>
2017-08-09 23:57:23 -0400

    Performed some coding optimization and also support sorting of strings.

--- Java/src/sort/MyBubbleSort.java
+++ Java/src/sort/MyBubbleSort.java
@@ -13,55 +13,101 @@ import java.util.Arrays;
  */
 public class MyBubbleSort {
     
-    public static void bubble_sort(int array[]) {
-        int n = array.length;
-        int k;
-        boolean isSorted = true;
+    public static void bubble_sort(int numArray[]) {
         
-        System.out.println ("\nInput array = " + Arrays.toString(array));
+        int cCount=0;  // how many time do we compare?
+        int sCount=0;  // how many time do we swap?
+        boolean swapped = true;
+        int last = numArray.length - 2;
         
-        for (int m = n; m >= 0; m--) {
-            isSorted = true;
+        while (swapped) {
+            swapped = false; //if we go though this loop and never swap, the array is sorted
             
-            for (int i = 0; i < m - 1; i++) {
-                k = i + 1;
-                
-                System.out.print ("[" + i + "] " + array[i] + " " + array [k]);
-                if (array[i] > array[k]) {
-                    isSorted = false;
-                    swapNumbers (i, k, array);
-                    System.out.print (" --> " + array[i] + ", " + array [k]);
-                }
-                System.out.println();
-            }
-            printNumbers (array);
-            
-            if (isSorted) {
-                System.out.println ("Array is sorted");
-                break;
-            }
+            for (int i=0; i<= last; i++) {
+                cCount++;
+                if (numArray[i] > numArray[i+1]) {
+                    int temp = numArray[i];
+                    numArray[i] = numArray[i+1];
+                    numArray[i+1] = temp;
+                    swapped = true;  //we did a swap, so the array is not sorted yet
+                    sCount++;
+                }  
+            }    
+            last--;
         }
+        // print out some stastical data
+        System.out.println("# of elements = " + numArray.length);
+        System.out.println("Compare Count = " + cCount);
+        System.out.println("Swap Count    = " + sCount);
     }
     
-    private static void swapNumbers (int i, int j, int[] array) {
-        int t;
+    /* In order to sort strings, we need to overload the class
+     * to accept string array as input, and use specific string
+     * comparison for evaluation
+    */
+    public static void bubble_sort(String[] strArray) {
         
-        t = array[i];
-        array[i] = array [j];
-        array[j] = t;
-    }
-    
-    private static void printNumbers (int[] input) {
-        for (int i = 0; i < input.length; i++) {
-            System.out.print (input[i] + " ");
+        int iCount=0;
+        int sCount=0;
+        boolean swapped = true;
+        int last = strArray.length - 2;
+        
+        while (swapped) {
+            swapped = false;
+            
+            for (int i=0; i<= last; i++) {
+                iCount++;
+                if (strArray[i].compareTo(strArray[i+1]) > 0) {
+                    String temp = strArray[i];
+                    strArray[i] = strArray[i+1];
+                    strArray[i+1] = temp;
+                    swapped = true;
+                    sCount++;
+                }  
+            }    
+            last--;
         }
-        System.out.println();
+        // print out some stastical data
+        System.out.println("Length= " + strArray.length);
+        System.out.println("iCount= " + iCount);
+        System.out.println("sCount= " + sCount);
     }
     
     public static void main (String [] args) {
-        int[] input = { 5, 10, 2, 8, 9, -4, 7, 20, 15, 6, -6 };
-        bubble_sort (input);
-        int [] input2 = { 3, 6, 8, 10, 2, 100, 1001};
+        
+        System.out.println ("--- Test Case 1 - random ordered numbers ---");
+        int[] input1 = { 5, 10, 2, 8, -4, 7, 20, -6 };
+        System.out.println ("Before Sorting = " + Arrays.toString(input1));
+        bubble_sort (input1);
+        System.out.println ("After Sorting = " + Arrays.toString(input1));
+        System.out.println();
+        
+        System.out.println ("--- Test Case 2 - random ordered numbers ---");
+        int[] input2 = { 3, 6, 8, 10, 2, 100, 1001, 5, 2, -6, 0};
+        System.out.println ("Before Sorting = " + Arrays.toString(input2));
         bubble_sort (input2);
+        System.out.println ("After Sorting = " + Arrays.toString(input2));
+        System.out.println();
+        
+        System.out.println ("--- Test Case 3 - already sorted");
+        int[] input3 = { 1, 3, 5, 7, 9, 11, 13, 15 };
+        System.out.println ("Before Sorting = " + Arrays.toString(input3));
+        bubble_sort (input3);
+        System.out.println ("After Sorting = " + Arrays.toString(input3));
+        System.out.println();
+           
+        System.out.println ("--- Test Case 4 - worse case");
+        int[] input4 = { 100, 90, 80, 75, 60, 53, 42, 39, 27, 15, 5, 0};
+        System.out.println ("Before Sorting = " + Arrays.toString(input4));
+        bubble_sort (input4);
+        System.out.println ("After Sorting = " + Arrays.toString(input4)); 
+        System.out.println();
+        
+        System.out.println ("--- Test Case 5 - Sorting strings");
+        String[] strInput1 = {"Elephant", "Dogs", "Zebra", "Cat", "Mouse" };
+        System.out.println ("Before Sorting = " + Arrays.toString(strInput1));
+        bubble_sort (strInput1);
+        System.out.println ("After Sorting = " + Arrays.toString(strInput1)); 
+        System.out.println();
     }
 }
